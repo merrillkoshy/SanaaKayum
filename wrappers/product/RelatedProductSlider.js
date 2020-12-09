4373fc48d117921a0696782a27e15a145b5db77a
merrillkoshy <merrillkoshy@gmail.com>
2020-12-09 14:29:28 +0400

    Rectified RelatedProdSlider and Searchbar bug on desktops

--- wrappers/product/RelatedProductSlider.js
+++ wrappers/product/RelatedProductSlider.js
@@ -1,30 +1,57 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Swiper from "react-id-swiper";
 import ProductGrid from "./ProductGrid";
+import OwlCarousel from "react-owl-carousel3";
+import Skeleton from "react-loading-skeleton";
 
 const RelatedProductSlider = ({ spaceBottomClass, category }) => {
-  
-  const settings = {
-    loop: false,
-    slidesPerView: 4,
-    grabCursor: true,
-    breakpoints: {
-      1024: {
-        slidesPerView: 4
-      },
-      768: {
-        slidesPerView: 3
-      },
-      640: {
-        slidesPerView: 2
-      },
-      320: {
-        slidesPerView: 2
-      }
-    }
-  };
+  const [display, setDisplay] = useState(false);
 
+  useEffect(()=>{
+    setDisplay(true)
+  })
+  const options = {
+    nav: true,
+    dots: false,
+    center: false,
+    touchDrag: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      330: {
+        items: 2
+      },
+      460: {
+        items: 3
+      },
+      600: {
+        items: 4
+      },
+      850: {
+        items: 5
+      },
+      1000: {
+        items: 6
+      },
+      1200: {
+        items: 7
+      },
+      1300: {
+        items: 8
+      },
+      1600: {
+        items: 8
+      }
+    },
+    mouseDrag: true,
+    navText: [
+      "<i class='pe-7s-angle-left'></i>",
+      "<i class='pe-7s-angle-right'></i>"
+    ]
+  };
   return (
     <div
       className={`related-product-area ${
@@ -35,14 +62,14 @@ const RelatedProductSlider = ({ spaceBottomClass, category }) => {
         <h2>Related Products</h2>
         
         <div className="row">
-          <Swiper {...settings}>
+        {display?<OwlCarousel  {...options} className="owl-theme" nav>
             
             <ProductGrid
               article={category}
               limit={6}
               sliderClassName="swiper-slide"
             />
-          </Swiper>
+          </OwlCarousel>:<Skeleton height={150} />}
         </div>
       </div>
     </div>
